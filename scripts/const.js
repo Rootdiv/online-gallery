6c71b0844d96e867f873c66675fd211478396aa5
Rootdiv <rootdiv@mail.ru>
2022-06-19 20:37:57 +0300

    feat: change reditect uri

--- scripts/const.js
+++ scripts/const.js
@@ -4,6 +4,6 @@ export const API_URL_TOKEN = 'https://unsplash.com/oauth/token';
 export const API_URL_AUTH = 'https://unsplash.com/oauth/authorize';
 export const ACCESS_KEY = 'Uhrr5NtSAEqp8ILiMMG8eFBSwKywg0cjR9fhauP0RS0';
 export const SECRET_KEY = '8lFkQst-AC2jRTqGYPR0_ESU2Ax79DqdUsTxPGScswQ';
-export const REDIRECT_URI = 'https://rootdiv.github.io/online-gallery/';
+export const REDIRECT_URI = 'https://online-gallery-gamma.vercel.app/';
 export const RESPONSE_TYPE = 'code';
 export const SCOPE = 'public read_user read_photos write_likes';
